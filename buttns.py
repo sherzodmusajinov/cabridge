d3ebacfe9004f78579c481f989cd6e0134fd5fd5
sherzodmusajinov <sherzod.musajinov@gmail.com>
2024-11-20 21:01:07 +0500

    Add files via upload

---
+++ buttns.py
@@ -0,0 +1,37 @@
+from aiogram.types import KeyboardButton,ReplyKeyboardMarkup
+
+
+book1 = KeyboardButton("Camridge 01 📕")
+book2 = KeyboardButton("Camridge 02 📗")
+book3 = KeyboardButton("Camridge 03 📔")
+book4 = KeyboardButton("Camridge 04 📚")
+book5 = KeyboardButton("Camridge 05 📕")
+book6 = KeyboardButton("Camridge 06 📗")
+book7 = KeyboardButton("Camridge 07 📔")
+book8 = KeyboardButton("Camridge 08 📚")
+book9 = KeyboardButton("Camridge 09 📕")
+book10 = KeyboardButton("Camridge 10 📗")
+book11 = KeyboardButton("Camridge 11 📔")
+book12 = KeyboardButton("Camridge 12 📚")
+book13 = KeyboardButton("Camridge 13 📕")
+book14 = KeyboardButton("Camridge 14 📗")
+book15 = KeyboardButton("Camridge 15 📔")
+book16 = KeyboardButton("Camridge 16 📚")
+book17 = KeyboardButton("Camridge 17 📕")
+book18 = KeyboardButton("Camridge 18 📗")
+
+
+menu = ReplyKeyboardMarkup()
+
+
+menu.add(book1, book2)
+menu.add(book3, book4, book5)
+menu.add(book6, book7, book8)
+menu.add(book9, book10, book11)
+menu.add(book12, book13, book14)
+menu.add(book15, book16)
+menu.add(book17, book18)
+
+
+
+
